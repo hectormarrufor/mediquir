cbd33217f65268c3d95a71c1434f89b2256005dd
Hector <hectormmarrufor@gmail.com>
2026-08-08 23:47:49 -0400

    mejorando inicio de sesion y encriptando contrase;as

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -47,7 +47,7 @@ export async function POST(request) {
     }
 
     // --- SOLUCIÓN: Encriptar la contraseña ---
-    const hashedPassword = await bcrypt.hash(password, parseInt(process.env.NEXT_PUBLIC_SALT_ROUNDS));
+    const hashedPassword = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS));
 
     // Creamos el usuario, pero sobreescribimos el password con la versión hasheada
     let nuevoUsuario = await User.create({
