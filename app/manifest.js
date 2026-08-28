f427ffe9edfc433d37b773da8c656405605204a3
Hector <hectormmarrufor@gmail.com>
2026-08-28 18:29:45 -0400

    resolviendo un bug del manifiesto

--- app/manifest.js
+++ app/manifest.js
@@ -8,8 +8,8 @@ export default function manifest() {
     description: isMediquir ? 'App para venta de insumos medicos Mediquir' : 'App para gestión logística Genérica',
     start_url: '/',
     display: 'standalone',
-    background_color: '#808080',
-    theme_color: '#0a0a0a',
+    "theme_color": "#1e4152",
+  "background_color": "#22303a",
     orientation: 'portrait',
     scope: '/',
     lang: 'es-VE',
@@ -18,13 +18,22 @@ export default function manifest() {
       {
         src: `/tenants/${clientId}/icons/icon-192x192.png`,
         sizes: '192x192',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'maskable'
       },
       {
         src: `/tenants/${clientId}/icons/icon-512x512.png`,
         sizes: '512x512',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'maskable'
+      },
+      {
+        src: `/tenants/${clientId}/icons/android-launchericon-96-96.png`,
+        sizes: '96x96',
+        type: 'image/png',
+        purpose: 'monochrome'
       }
+  
     ]
   };
 }
